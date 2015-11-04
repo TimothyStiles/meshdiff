6406e574450cc3e54d7ad72e57648ac85dcf7479
TimothyStiles <TimothySStiles@gmail.com>
2015-11-04 12:37:27 -0500

    app.js now creates input filepath from input arguments.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+#!/usr/bin/env node
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var op = require('open');
+var path = require('path');
+
+var fileName1 = path.normalize(process.argv[2]),
+    fileName2 = path.normalize(process.argv[3]),
+    filePath1 = path.join(process.cwd(), fileName1),
+    filePath2 = path.join(process.cwd(), fileName2);
+   
+console.log(filePath1, filePath2);
+
+console.log(process.argv[2]);
+console.log(process.cwd());
 
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');
@@ -15,13 +28,11 @@ io.on('connection', function(socket) {
   console.log('a user connected');
 });
 
-
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-
 http.listen(port, ip_address, function(){
-    console.log( "Listening on " + ip_address + ", server_port " + port );
+    console.log( "Listening on:");
     console.log('http://'+ ip_address  + ':' + port + '/');
     op('http://'+ ip_address  + ':' + port + '/');
 });
