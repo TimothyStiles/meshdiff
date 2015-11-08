edc457992b3112b6abf8b09b43ef1e94d1c29212
TimothyStiles <TimothySStiles@gmail.com>
2015-11-08 17:58:35 -0500

    created client script to render 3D scene.

---
+++ js/client.js
@@ -0,0 +1,121 @@
+var socket = require('socket.io-client')('http://127.0.0.1:8080/');
+var THREE = require('three');
+
+var camera, cameraTarget, scene, renderer;
+
+var containers;
+
+init();
+animate();
+
+function init() {
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+
+	camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 1, 15 );
+	camera.position.set( 3, 0.15, 3 );
+
+	cameraTarget = new THREE.Vector3( 0, -0.25, 0 );
+
+
+	scene = new THREE.Scene();
+	scene.fog = new THREE.Fog( 0x72645b, 2, 15 );
+
+
+	// Ground
+
+	var plane = new THREE.Mesh(
+		new THREE.PlaneBufferGeometry( 40, 40 ),
+		new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
+	);
+	plane.rotation.x = -Math.PI/2;
+	plane.position.y = -0.5;
+	scene.add( plane );
+
+	plane.receiveShadow = true;
+
+
+	// Lights
+
+	scene.add( new THREE.HemisphereLight( 0x443333, 0x111122 ) );
+
+	addShadowedLight( 0.5, 1, -1, 0xffff99, 1 );
+
+	// renderer
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setClearColor( scene.fog.color );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	renderer.gammaInput = true;
+	renderer.gammaOutput = true;
+
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.cullFace = THREE.CullFaceBack;
+
+	container.appendChild( renderer.domElement );
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function addShadowedLight( x, y, z, color, intensity ) {
+
+	var directionalLight = new THREE.DirectionalLight( color, intensity );
+	directionalLight.position.set( x, y, z );
+	scene.add( directionalLight );
+
+	directionalLight.castShadow = true;
+	// directionalLight.shadowCameraVisible = true;
+
+	var d = 1;
+	directionalLight.shadowCameraLeft = -d;
+	directionalLight.shadowCameraRight = d;
+	directionalLight.shadowCameraTop = d;
+	directionalLight.shadowCameraBottom = -d;
+
+	directionalLight.shadowCameraNear = 1;
+	directionalLight.shadowCameraFar = 4;
+
+	directionalLight.shadowMapWidth = 1024;
+	directionalLight.shadowMapHeight = 1024;
+
+	directionalLight.shadowBias = -0.005;
+
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+	render();
+
+}
+
+function render() {
+
+	camera.lookAt( cameraTarget );
+
+	renderer.render( scene, camera );
+
+}
+
+socket.on('connect', function(){
+  console.log('it works');
+});
+
+socket.on('mesh', function(data){
+  scene.addObject(mesh);
+
+});
+socket.on('disconnect', function(){});
