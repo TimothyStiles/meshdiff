19426dc09879c3a9e323325dd1cab416d6e23717
TimothyStiles <TimothySStiles@gmail.com>
2015-11-17 18:40:32 -0500

    removed triangle example and unneeded local dependency from app.js.

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var op = require('open');
 var THREE = require('three');
-//var objparse = require('./js/ObjectLoader');
 
 var fileName1 = path.normalize(process.argv[2]),
     fileName2 = path.normalize(process.argv[3]),
@@ -15,12 +14,9 @@ var fileName1 = path.normalize(process.argv[2]),
     filePath2 = path.resolve(process.cwd(), fileName2);
    
 //load json files. In future will modularize and include more formats.
-
 var parseModel = new THREE.ObjectLoader();
 
-
 //should be moved to another file and imported.
-
 function handleFile(err, data) {
   if (err) throw err;
   var model = parseModel.parse(JSON.parse(data));
@@ -44,20 +40,6 @@ app.get('/favicon.ico', function(req, res){
 
 io.on('connection', function(socket) {
   console.log('a user connected');
-  var cube = new THREE.CubeGeometry( 1, 1, 1 );
-  var geom = new THREE.Geometry();
-  var v1 = new THREE.Vector3(0,0,0);
-  var v2 = new THREE.Vector3(30,0,0);
-  var v3 = new THREE.Vector3(30,30,0);
-
-  geom.vertices.push(v1);
-  geom.vertices.push(v2);
-  geom.vertices.push(v3);
-
-  geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
-  geom.computeFaceNormals();
-  var mesh = new THREE.Mesh(geom, new THREE.MeshNormalMaterial());
- // socket.emit('mesh', mesh.toJSON());
   fs.readFile(filePath1, handleFile);
 });
 
