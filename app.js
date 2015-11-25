9f0a1396b58201874f6a07dee2a6976a76cb28d9
TimothyStiles <TimothySStiles@gmail.com>
2015-11-24 20:57:28 -0500

    cleaned up app.js and set filepaths to export mesh files to client.

--- app.js
+++ app.js
@@ -1,48 +1,36 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var op = require('open');
-var THREE = require('three');
-var stl = require('./js/loaders/STLLoader.js');
-var Buffer = require('buffer').Buffer;
-var Iconv = require('iconv').Iconv;
+var fs = require('fs'),
+    path = require('path'),
+    app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    op = require('open'),
+    THREE = require('three');
 
 var fileName1 = path.normalize(process.argv[2]),
     fileName2 = path.normalize(process.argv[3]),
     filePath1 = path.resolve(process.cwd(), fileName1),
-    filePath2 = path.resolve(process.cwd(), fileName2);
-   
-//load json files. In future will modularize and include more formats.
-var parseModel = new THREE.ObjectLoader();
-var stlModel = new stl();
-
-
-//should be moved to another file and imported.
-function handleFile(err, data) {
-  if (err) throw err;
-  var model = parseModel.parse(JSON.parse(data));
-//  var iconv = new Iconv('UTF-8', 'UTF-32');
-//  var buf = iconv.convert(data);
-//  var arrayBuffer = new Float32Array(buf).buffer;
-//  var model = stlModel.parse(arrayBuffer); 
-//  var mesh = new THREE.Mesh(model, new THREE.MeshNormalMaterial());
-//  console.log(mesh); 
-  io.emit('mesh', model.toJSON());
-}
-
+    filePath2 = path.resolve(process.cwd(), fileName2),
+    baseName1 = '/' + path.basename(fileName1),
+    baseName2 = '/' + path.basename(fileName2); 
 
 //serve client files
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
 app.get('/bundle.js', function(req, res){
   res.sendFile(__dirname + '/bundle.js');
+});
+
+app.get(baseName1, function(req, res){
+  res.sendFile(filePath1);
+});
+
+app.get(baseName2, function(req, res){
+  res.sendFile(filePath2);
 });
 
 app.get('/favicon.ico', function(req, res){
@@ -50,8 +38,9 @@ app.get('/favicon.ico', function(req, res){
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
-  fs.readFile(filePath1, "utf-8", handleFile);
+  console.log('We have made contact...');
+  socket.emit('mesh1', baseName1);
+  socket.emit('mesh2', baseName2);
 });
 
 
